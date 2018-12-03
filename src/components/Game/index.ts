48fd276b040b7980dbc54496f4b5bbdd85e36b61
Leonid Belikov <mail@arfeo.net>
2018-12-04 02:13:50 +0300

    game window render

--- src/components/Game/index.ts
+++ src/components/Game/index.ts
@@ -1,16 +1,39 @@
-import { renderGameBoard } from './render';
+import { renderGameWindow, renderPanelCounters } from './render';
 
 class Game {
   appRoot: HTMLElement;
+  level: number;
+  lives: number;
+  score: number;
+  boardPanel: {
+    level: HTMLElement;
+    lives: HTMLElement;
+    score: HTMLElement;
+  };
 
-  constructor() {
+  constructor(level = 1, lives = 5, score = 0) {
     this.appRoot = document.getElementById('root');
+
+    this.level = level;
+    this.lives = lives;
+    this.score = score;
+
+    this.boardPanel = {
+      level: document.createElement('div'),
+      lives: document.createElement('div'),
+      score: document.createElement('div'),
+    };
+
+    this.boardPanel.level.className = '-level';
+    this.boardPanel.lives.className = '-lives';
+    this.boardPanel.score.className = '-score';
 
     this.render();
   }
 
   render() {
-    renderGameBoard.call(this);
+    renderGameWindow.call(this);
+    renderPanelCounters.call(this);
   }
 }
 
--- src/components/Game/render.ts
+++ src/components/Game/render.ts
@@ -1,9 +1,24 @@
-function renderGameBoard() {
-  const gameBoard: HTMLElement = document.createElement('div');
+function renderGameWindow() {
+  const gameWindow: HTMLElement = document.createElement('div');
+  const boardPanel: HTMLElement = document.createElement('div');
+  const boardGrid: HTMLElement = document.createElement('div');
 
-  gameBoard.classList.add('gameBoard');
+  gameWindow.classList.add('gameWindow');
+  boardPanel.classList.add('boardPanel');
+  boardGrid.classList.add('boardGrid');
 
-  this.appRoot.appendChild(gameBoard);
+  this.appRoot.appendChild(gameWindow);
+  gameWindow.appendChild(boardPanel);
+  boardPanel.appendChild(this.boardPanel.level);
+  boardPanel.appendChild(this.boardPanel.lives);
+  boardPanel.appendChild(this.boardPanel.score);
+  gameWindow.appendChild(boardGrid);
 }
 
-export { renderGameBoard };
+function renderPanelCounters() {
+  this.boardPanel.level.innerText = this.level;
+  this.boardPanel.lives.innerText = this.lives;
+  this.boardPanel.score.innerText = this.score;
+}
+
+export { renderGameWindow, renderPanelCounters };
